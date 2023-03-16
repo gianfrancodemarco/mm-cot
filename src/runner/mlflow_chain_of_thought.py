4d00cd1cf28b0fa30287a9527bf1b91b27bc4a76
Gianfranco Demarco <gianfrademarco@gmail.com>
2023-03-16 23:35:55 +0100

    feat: mlflow logging as decorator

--- src/runner/mlflow_chain_of_thought.py
+++ src/runner/mlflow_chain_of_thought.py
@@ -2,26 +2,36 @@ from src.runner.chain_of_thought import ChainOfThought
 import mlflow
 
 
-class MLFlowChainOfThought(ChainOfThought):
-
-    def evaluate(self) -> dict:
+def mlflow_logging(func):
+    def wrapper(*args, **kwargs) -> dict:
         """ 
         Generate the textual output for the dataset and returns the metrics
         Logs the experiment on MLFlow
         """
 
-        run_name = f"{self.args.dataset} || {self.filename} || {self.args.img_type}".upper()
-        with mlflow.start_run(run_name=run_name):
+        instance = args[0]
+        with mlflow.start_run():
             # model = mlflow.pytorch.load_model(self.args.model + '/pytorch_model.bin')
             # mlflow.pytorch.log_model(model, "model")
 
-            metrics = super().evaluate()
+            metrics = func(instance)
 
             log_params = {
-                "metrics": metrics,
-                "img_type": self.args.img_type,
-                "output": self.filename,
+                **metrics,
+                "task": instance.args.task,
+                "dataset": instance.args.dataset,
+                "img_type": instance.args.img_type,
+                "output": instance.filename,
+                "test_le": instance.args.test_le
             }
 
             mlflow.log_params(log_params)
             mlflow.end_run()
+    return wrapper
+
+
+class MLFlowChainOfThought(ChainOfThought):
+
+    @mlflow_logging
+    def evaluate(self) -> dict:
+        return super().evaluate()
