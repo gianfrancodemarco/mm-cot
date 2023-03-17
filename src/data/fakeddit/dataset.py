3d603c498900af3b4a849cf038235e0c417eab18
Gianfranco Demarco <gianfrademarco@gmail.com>
2023-03-17 20:08:23 +0100

    feat: fakeddit prediction with rationale

--- src/data/fakeddit/dataset.py
+++ src/data/fakeddit/dataset.py
@@ -1,5 +1,5 @@
 
-from typing import Tuple
+from typing import List, Tuple
 
 import numpy as np
 import pandas as pd
@@ -7,34 +7,17 @@ import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 from transformers import T5Tokenizer
-from src.data.fakeddit.labels import (LabelsTypes, get_label_column,
-                                      get_options_text, convert_int_to_label, get_label_text)
-# test_le: Probably it is the previously generated rationale, needed to inference the answer (so it will be null when)
-# inferencing the rationale
 
-# prompt: The question
-#   e.i: "Question: What does the verbal irony in this text suggest?\nAccording to Mr. Herrera's kids, his snoring is as quiet as a jackhammer.\nContext: N/A\nOptions: (A) The snoring is loud. (B) The snoring occurs in bursts.\nSolution:"
-# target: The rationale
-#   e.i: "Solution: Figures of speech are words or phrases that use language in a nonliteral or unusual way. They can make writing more expressive.\\nVerbal irony involves saying one thing but implying something very different. People often use verbal irony when they are being sarcastic.\\nOlivia seems thrilled that her car keeps breaking down.\\nEach breakdown is as enjoyable as a punch to the face. The text uses verbal irony, which involves saying one thing but implying something very different.\\nAs quiet as a jackhammer suggests that the snoring is loud. A jackhammer is not quiet, and neither is Mr. Herrera's snoring.."
+from src.data.fakeddit.labels import (LabelsTypes, convert_int_to_label,
+                                      get_label_column, get_label_text,
+                                      get_options_text)
 
-# source = self.process_data(prompt, self.source_len)
-# source_id = source["input_ids"].squeeze().to(device)
-# source_mask = source["attention_mask"].squeeze().to(device)
-# target = target["input_ids"].squeeze().to(device)
-
-
-# "input_ids": self.source_ids[index].to(torch.long),
-# "attention_mask": self.source_masks[index].to(torch.long),
-# "image_ids": self.image_ids[index].to(torch.float),
-# "labels": self.target_ids[index].to(torch.long).tolist(),
-
-#  \nContext: A news is a piece of information regardings fact happening in the world. A news can also be crafted and manipulated with malicious objectives.
 DATASET_PATH = 'data/fakeddit/partial/dataset.csv'
 DEFAULT_PROMPT = """Question: Which of the given options better reflects the news given? \nContext: A news is a piece of information regardings fact happening in the world. A news can also be crafted and manipulated with malicious objectives. \n<TEXT>\nOptions: <OPTIONS>"""
 
 IMG_SHAPE = (100, 256)
 
-device = 'cpu' #'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cpu'  # 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 class FakedditDataset(Dataset):
@@ -43,9 +26,10 @@ class FakedditDataset(Dataset):
         self,
         dataframe: pd.DataFrame,
         tokenizer: T5Tokenizer,
-        max_length: int = 512,
         vision_features: np.ndarray = None,
-        labels_type: LabelsTypes = LabelsTypes.TWO_WAY
+        rationales: List[str] = None,
+        labels_type: LabelsTypes = LabelsTypes.TWO_WAY,
+        max_length: int = 512
     ) -> None:
 
         self.labels_type = labels_type
@@ -53,6 +37,7 @@ class FakedditDataset(Dataset):
         self.tokenizer = tokenizer
         self.max_length = max_length
         self.vision_features = vision_features
+        self.rationales = rationales
 
         self.input_ids = torch.tensor([], device=device)
         self.attention_masks = torch.tensor([], device=device)
@@ -67,7 +52,12 @@ class FakedditDataset(Dataset):
     def _build_dataset(self) -> None:
 
         for index, row in enumerate(self.dataframe.to_dict(orient="records")[:500]):
-            _input_ids, _attention_mask = self.get_input_ids(row)
+            
+            _rationale = ''
+            if self.rationales:
+                _rationale  = self.rationales[index]
+            
+            _input_ids, _attention_mask = self.get_input_ids(row["clean_title"], _rationale)
 
             self.input_ids = torch.cat(
                 (self.input_ids, _input_ids.unsqueeze(0)), 0)
@@ -79,10 +69,9 @@ class FakedditDataset(Dataset):
                 self.image_ids = torch.cat(
                     (self.image_ids, _image_ids.unsqueeze(0)), 0)
 
-    def get_input_ids(self, row: dict) -> Tuple[Tensor, Tensor]:
+    def get_input_ids(self, title: str, rationale: str) -> Tuple[Tensor, Tensor]:
 
-        clean_title = row["clean_title"]
-        full_text = self._get_question_text(clean_title)
+        full_text = self._get_question_text(title, rationale)
         processed = self.process_data(full_text)
 
         input_ids = processed["input_ids"].squeeze().to(device)
@@ -90,11 +79,12 @@ class FakedditDataset(Dataset):
 
         return input_ids, attention_mask
 
-    def _get_question_text(self, clean_title: str) -> str:
+    def _get_question_text(self, title: str, rationale: str) -> str:
         options_text = get_options_text(self.labels_type)
 
-        question_text = DEFAULT_PROMPT.replace("<TEXT>", clean_title)
+        question_text = DEFAULT_PROMPT.replace("<TEXT>", title)
         question_text = question_text.replace("<OPTIONS>", options_text)
+        question_text = "\n".join([question_text, rationale])
 
         return question_text
 
@@ -137,7 +127,7 @@ class FakedditDataset(Dataset):
         item = {
             "input_ids": self.input_ids[index].to(torch.long),
             "attention_mask": self.attention_masks[index].to(torch.long),
-            #"labels":  self.get_label(index),
+            # "labels":  self.get_label(index),
             "plain_labels": get_label_text(convert_int_to_label(self.get_label(index)))
         }
 
@@ -145,7 +135,7 @@ class FakedditDataset(Dataset):
             item = {
                 **item,
                 "image_ids": self.image_ids[index].to(torch.float)
-                #"image_ids": torch.zeros(IMG_SHAPE).to(torch.float) FOR EXCLUDE VISION FEATURES
+                # "image_ids": torch.zeros(IMG_SHAPE).to(torch.float) FOR EXCLUDE VISION FEATURES
             }
 
         return item
